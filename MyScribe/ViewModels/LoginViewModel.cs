3a2987038ffb8d2d3f2018ff1d88c097f0aa2e87
Todd Walraven <cod4247@yahoo.com>
2020-04-06 13:44:57 -0700

    update login view model
    Co-authored-by: Keturah Howard <keturah.dev@gmail.com>
    Co-authored-by: Jose Amesquita <joseamesquita11@gmail.com>
    Co-authored-by: James Carlos <jonathancarlos21@hotmail.com>

--- MyScribe/ViewModels/LoginViewModel.cs
+++ MyScribe/ViewModels/LoginViewModel.cs
@@ -1,8 +1,8 @@
 namespace MyScribe.ViewModels
 {
     public class LoginViewModel
-  {
-    public string Email { get; set; }
-    public string Password { get; set; }
-  }
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
 }
