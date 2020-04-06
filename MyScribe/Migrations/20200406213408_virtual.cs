e2998cb37a8a69914c294bfe60c7570570a6d047
Todd Walraven <cod4247@yahoo.com>
2020-04-06 14:34:47 -0700

    update database
    Co-authored-by: Keturah Howard <keturah.dev@gmail.com>
    Co-authored-by: Jose Amesquita <joseamesquita11@gmail.com>
    Co-authored-by: James Carlos <jonathancarlos21@hotmail.com>

---
+++ MyScribe/Migrations/20200406213408_virtual.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyScribe.Migrations
+{
+    public partial class @virtual : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Boards",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Boards");
+        }
+    }
+}
