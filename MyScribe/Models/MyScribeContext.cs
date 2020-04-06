df14ee646e0402f1a7a822389fb03c633f124c6f
Todd Walraven <cod4247@yahoo.com>
2020-04-06 16:15:59 -0700

    comment out seed data in context model
    Co-authored-by: Keturah Howard <keturah.dev@gmail.com>
    Co-authored-by: Jose Amesquita <joseamesquita11@gmail.com>
    Co-authored-by: James Carlos <jonathancarlos21@hotmail.com>

--- MyScribe/Models/MyScribeContext.cs
+++ MyScribe/Models/MyScribeContext.cs
@@ -8,5 +8,21 @@ namespace MyScribe.Models
     public DbSet<Board> Boards { get; set; }
     public DbSet<Post> Posts { get; set; }
     public MyScribeContext(DbContextOptions options) : base(options) { }
+
+    // protected override void OnModelCreating(ModelBuilder builder)
+    //   {
+    //     Builder.Entity<Boards>()
+    //     {
+    //       .HasData(
+    //         new Board {BoardId = 1, UserId = 1 Name = "ThoughtBoard", Description = "this is an example board to demonstrate what your ScriBoards can look like!", BannerImage="http://d1hkwj6qq9t78x.cloudfront.net/static/ui/longphoto-clockone-001b.jpg"}
+    //       )
+    //     }
+    //     Builder.Entity<ApplicationUser>()
+    //     {
+    //       .HasData(
+    //         new ApplicationUser {UserId= "1", Email=""}
+    //       )
+    //     }
+    // }
   }
 }
