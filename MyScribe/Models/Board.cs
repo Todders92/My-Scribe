21a451f11a3887fdfa5f2c0a89a34296b421bfb7
Todd Walraven <cod4247@yahoo.com>
2020-04-06 13:58:48 -0700

    add name field to board model
    Co-authored-by: Keturah Howard <keturah.dev@gmail.com>
    Co-authored-by: Jose Amesquita <joseamesquita11@gmail.com>
    Co-authored-by: James Carlos <jonathancarlos21@hotmail.com>

--- MyScribe/Models/Board.cs
+++ MyScribe/Models/Board.cs
@@ -9,6 +9,7 @@ namespace MyScribe.Models
     }
     public int BoardId { get; set; }
     public int UserId { get; set; }
+    public string Name { get; set; }
     public string Description { get; set; }
     public string BannerImage { get; set; }
     public ICollection<Post> Posts {get;set;}
