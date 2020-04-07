bb50410a2ad9459927f71bfab90c377004a4c4b6
Todd Walraven <cod4247@yahoo.com>
2020-04-07 10:46:38 -0700

    update database
    Co-authored-by: Keturah Howard <keturah.dev@gmail.com>
    Co-authored-by: Jose Amesquita <joseamesquita11@gmail.com>
    Co-authored-by: James Carlos <jonathancarlos21@hotmail.com>

---
+++ MyScribe/Migrations/20200407173929_modelUpdate.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyScribe.Migrations
+{
+    public partial class modelUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Posts");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Posts",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
