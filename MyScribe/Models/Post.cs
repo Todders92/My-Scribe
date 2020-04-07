0a13f73197f9b054c1ff6d7ae1b589014a8eb257
Todd Walraven <cod4247@yahoo.com>
2020-04-07 10:48:02 -0700

    update post model
    Co-authored-by: Keturah Howard <keturah.dev@gmail.com>
    Co-authored-by: Jose Amesquita <joseamesquita11@gmail.com>
    Co-authored-by: James Carlos <jonathancarlos21@hotmail.com>

--- MyScribe/Models/Post.cs
+++ MyScribe/Models/Post.cs
@@ -4,7 +4,6 @@ namespace MyScribe.Models
   {
     public int PostId { get; set; }
     public int BoardId { get;set; }
-    public int UserId { get; set; }
     public string Title { get; set; }
     public string Description { get; set; }
     public string ImageSource { get; set; }
