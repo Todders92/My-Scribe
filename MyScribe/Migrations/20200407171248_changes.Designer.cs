fdf4e2787849b589b22f3b336df9bc892375c7b4
Todd Walraven <cod4247@yahoo.com>
2020-04-07 10:16:26 -0700

    update database
    Co-authored-by: Keturah Howard <keturah.dev@gmail.com>
    Co-authored-by: Jose Amesquita <joseamesquita11@gmail.com>
    Co-authored-by: James Carlos <jonathancarlos21@hotmail.com>

--- MyScribe/Migrations/20200407171248_changes.Designer.cs
+++ MyScribe/Migrations/20200407171248_changes.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyScribe.Models;
 
 namespace MyScribe.Migrations
 {
     [DbContext(typeof(MyScribeContext))]
-    partial class MyScribeContextModelSnapshot : ModelSnapshot
+    [Migration("20200407171248_changes")]
+    partial class changes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MyScribe/Migrations/20200407171248_changes.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyScribe.Migrations
+{
+    public partial class changes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Boards",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Boards_UserId",
+                table: "Boards",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Boards_AspNetUsers_UserId",
+                table: "Boards",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Boards_AspNetUsers_UserId",
+                table: "Boards");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Boards_UserId",
+                table: "Boards");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Boards",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
