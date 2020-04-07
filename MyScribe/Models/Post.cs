b1c57fc5b849fa9d4832f533a8d7740d6fd1126f
Todd Walraven <cod4247@yahoo.com>
2020-04-07 11:04:51 -0700

    update field names in post model
    Co-authored-by: Keturah Howard <keturah.dev@gmail.com>
    Co-authored-by: Jose Amesquita <joseamesquita11@gmail.com>
    Co-authored-by: James Carlos <jonathancarlos21@hotmail.com>

--- MyScribe/Models/Post.cs
+++ MyScribe/Models/Post.cs
@@ -5,9 +5,9 @@ namespace MyScribe.Models
     public int PostId { get; set; }
     public int BoardId { get;set; }
     public string Title { get; set; }
-    public string Description { get; set; }
+    public string Body { get; set; }
     public string ImageSource { get; set; }
-    public string Links { get; set; }
+    public string Link { get; set; }
     public virtual Board Board { get; set; }
   }
 }
