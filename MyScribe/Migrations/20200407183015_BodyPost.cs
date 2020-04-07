2258b7567a159e0022445bfd729f2685c6140af0
Todd Walraven <cod4247@yahoo.com>
2020-04-07 11:30:52 -0700

    update database
    Co-authored-by: Keturah Howard <keturah.dev@gmail.com>
    Co-authored-by: Jose Amesquita <joseamesquita11@gmail.com>
    Co-authored-by: James Carlos <jonathancarlos21@hotmail.com>

---
+++ MyScribe/Migrations/20200407183015_BodyPost.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyScribe.Migrations
+{
+    public partial class BodyPost : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Links",
+                table: "Posts",
+                newName: "Link");
+
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Posts",
+                newName: "Body");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Link",
+                table: "Posts",
+                newName: "Links");
+
+            migrationBuilder.RenameColumn(
+                name: "Body",
+                table: "Posts",
+                newName: "Description");
+        }
+    }
+}
