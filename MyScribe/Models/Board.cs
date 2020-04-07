9023d3c419afec07212d7fd39b2c3f2bdfd114c4
Todd Walraven <cod4247@yahoo.com>
2020-04-07 10:22:20 -0700

    update model to hold user object
    Co-authored-by: Keturah Howard <keturah.dev@gmail.com>
    Co-authored-by: Jose Amesquita <joseamesquita11@gmail.com>
    Co-authored-by: James Carlos <jonathancarlos21@hotmail.com>

--- MyScribe/Models/Board.cs
+++ MyScribe/Models/Board.cs
@@ -8,10 +8,10 @@ namespace MyScribe.Models
       this.Posts = new HashSet<Post>();
     }
     public int BoardId { get; set; }
-    public int UserId { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
     public string BannerImage { get; set; }
+    public ApplicationUser User { get; set; }
     public ICollection<Post> Posts {get;set;}
   }
 }
