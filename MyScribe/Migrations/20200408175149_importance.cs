ce4f18c92aa4fcb4188890c92f2c4bf212416d88
Todd Walraven <cod4247@yahoo.com>
2020-04-08 10:52:45 -0700

    update database to have pinned posts functionality
    Co-authored-by: Keturah Howard <keturah.dev@gmail.com>
    Co-authored-by: Jose Amesquita <joseamesquita11@gmail.com>
    Co-authored-by: James Carlos <jonathancarlos21@hotmail.com>

---
+++ MyScribe/Migrations/20200408175149_importance.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyScribe.Migrations
+{
+    public partial class importance : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Importance",
+                table: "Posts",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Importance",
+                table: "Posts");
+        }
+    }
+}
